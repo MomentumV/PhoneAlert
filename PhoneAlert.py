08b840061aff8d21b411d86a6716fc825e111b28
Mordecai Veldt <momentumv@users.noreply.github.com>
2017-04-17 10:29:59 -0400

    split settings into two files, make config reload while running

--- PhoneAlert.py
+++ PhoneAlert.py
@@ -4,74 +4,49 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 from twilio.rest import Client
 
-#retrieve config file with secrets
-config = yaml.safe_load(open("config.yml"))
-#print(config)
-#with open("config.yml",'r') as ymlfile:
-#    cfg = yaml.load(ymlfile)
-#twitdict= cfg['twitter_set']
-twitdict = config['twitter_set']
-#twildict= cfg['twilio_set']
-twildict = config['twilio_set']
-searchdict = config['search_set']
-#store twitter secrets in variables
-access_token = twitdict['access_token']
-access_token_secret = twitdict['access_secret']
-consumer_key = twitdict['consumer_key']
-consumer_secret = twitdict['consumer_secret']
+def load_secrets():
+    with open("secrets.yml") as secrets_file:
+        secrets = yaml.safe_load(secrets_file)
+    return secrets
 
-#store twilio secrets in variables
-account = twildict['account']
-token = twildict['token']
-
-#get a twilio token
-
-
-#store call details from config in variables
-tophone = twildict['tophone']
-errorphone = twildict['err_notification']
-fromphone = twildict['fromphone']
-callurl = twildict['url']
-
-#create client object to use for calls or texts
-client = Client(account,token)
-
-#store search parameters
-ulist = searchdict['user']
-tlist = searchdict['track']
-exclude = searchdict['not']
-
-#define message strings
-errtextmess = 'error code: {0}'
-ratelimitmess = '420 error! chill out, man! we are down.'
+def load_settings():
+    with open("config.yml") as settings_file:
+        config = yaml.safe_load(settings_file)
+    return config
 
 #define a basic listener
 class CallListener(StreamListener):
 
     def on_data(self, data):
-        print 'matched tweet'
+        print 'matched tweet based on filtered user id'
         jsondata=json.loads(data)
+        #write data to disk; serves to prove the script is working/running
         with open('data.txt','w') as outfile:
             json.dump(jsondata,outfile)
-        #pdb.set_trace()
         userid = jsondata['user']['id_str']
         if not userid in ulist:
             print 'not one of our users'
-            return True
+            return True #return True continues the listener
+        #reload the track list and exclude lists
+        config = load_settings()
+        searchdict = config['search_set']
+        tlist = searchdict['track']
+        exclude = searchdict['not']
         tweettext=jsondata['text']
         print tweettext.encode('utf-8')
         #pdb.set_trace()
         trigger = 0
+        lowered_tweet_text = tweettext.lower()
         for word in tlist:
-            if word in tweettext.lower():
+            if word in lowered_tweet_text:
                 print 'keyword {0} found'.format(word)
                 trigger += 1
         for word in exclude:
-            if word in tweettext.lower():
+            if word in lowered_tweet_text:
                 print 'excluded string {0} found'.format(word)
                 trigger = -1
         if trigger > 0:
-            #print 'make call to '+tophone+' from '+fromphone
+            print 'make call to {0} from {1}'.format(tophone,fromphone)
             #make phone call first
             call = client.calls.create(to=tophone, from_=fromphone, url=callurl)
             #send text message containg the twitter message to tophone
@@ -92,12 +67,48 @@ class CallListener(StreamListener):
         else:
             message = client.api.account.messages.create(to=errorphone,from_=fromphone,body=textmess.format(code))
             return False
-if __name__ == '__main__':
 
+if __name__ == '__main__':
+    #load settings
+    #retrieve secrets and configuration  files
+    secrets = load_secrets()
+    config = load_settings()
+    twitsec = secrets['twitter_set']
+    twilsec = secrets['twilio_set']
+    
+    #store twitter secrets in variables
+    access_token = twitsec['access_token']
+    access_token_secret = twitsec['access_secret']
+    consumer_key = twitsec['consumer_key']
+    consumer_secret = twitsec['consumer_secret']
+    #store twilio secrets in variables
+    account = twilsec['account']
+    token = twilsec['token']
+    
+    #store configuration details in variables.
+    searchdict = config['search_set']
+    settings = config['settings']
+    
+    #store search parameters
+    ulist = searchdict['user']
+    tlist = searchdict['track']
+    exclude = searchdict['not']
+    
+    #define message settings
+    tophone = settings['tophone']
+    errorphone = settings['err_notification']
+    fromphone = settings['fromphone']
+    callurl = settings['url']
+    errtextmess = settings['err_txt'] #'error code: {0}'
+    ratelimitmess = settings['rate_lim_txt'] #'420 error! chill out, man! we are down.'
+    
+    #create twilio client object to use for calls or texts
+    client = Client(account,token)
+    
     #This handles Twitter authetification and the connection to Twitter Streaming API
-    l = CallListener()
+    listener = CallListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
-    stream = Stream(auth, l)
+    stream = Stream(auth, listener)
     #This line filter Twitter Streams to capture tweets from the user ids listed in ulist, which are pulled in from the config file. use the get_ids.py helper script to extract user ids from @handles
     stream.filter(follow=ulist,async=True)
--- README.md
+++ README.md
@@ -3,9 +3,12 @@
  ## Setup instructions ##
    - This is a Python app; you'll need to have a Python 2.7 installation.
    - You will need to create both a twitter app and a Twilio free user trial
-   - Enter your api keys from twitter and twilio, as well as the search terms into config.yml.sample, save it as config.yml.
+   - Enter your api keys from twitter and twilio into secrets.yml.sample and save it as secrets.yml
+   - Enter your phone numbers, search settings into config.yml.sample, save it as config.yml
+
 
  ## helpful hints ##
+   - the streaming.py for tweepy has some important improvements since the 3.5 version available through pip.
    - get_ids.py is a little script that returns the twitter user id from an entered twitter @handle
-   - sms notification is used when your streaming app is rate limited (code 420... chill out; seriously, that's what twitter calls it). Plan to only start ~2 streams per day; save some data and work with that for testing.
+   - sms notification is used when your streaming app is rate limited  from too many started streams (code 420... chill out; seriously, that's what twitter calls it). Plan to only start ~2 streams per day; save some data and work with that for testing.
 
